04904a9b56077373af0a9451361e9de377b75db2
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-16 10:49:25 +0100

    New commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // express app
 const app  = express()
 
+// routes
+app.get('/', (req, res) =>{
+    res.json({mssg: 'SOPE OTILOR'})
+})
+
 // listen to requests
 app.listen(4000, () => {
     console.log('listening to port 4000');
