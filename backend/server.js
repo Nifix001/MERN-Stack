98ffe14381b5efe467b46b256a3f2e25c0ac7795
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-17 22:35:32 +0100

    New commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
-
 const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app  = express()
 
-
 // middleware
-app.use(express.json)
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
@@ -24,4 +22,3 @@ app.listen(process.env.PORT, () => {
     console.log('listening to port 4000', process.env.PORT);
 })
 
-process.env
