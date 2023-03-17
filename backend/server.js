d2cd00e09fec19326e8b347b01f4ac338e97eb9b
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-17 23:32:54 +0100

    New commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -17,8 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen to requests
-app.listen(process.env.PORT, () => {
-    console.log('listening to port 4000', process.env.PORT);
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    // listen to requests
+    app.listen(process.env.PORT, () => {
+        console.log('listening to port 4000', process.env.PORT);
+    })
+})
+.catch((error) => {
+    console.log(error);
 })
 
