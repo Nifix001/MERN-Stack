d8261c74c0dcc1d92ede6157d9e4591afd86b908
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-17 21:49:47 +0100

    New commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const workoutRoutes = require('./routes/workouts')
 // express app
 const app  = express()
 
+
+// middleware
+app.use(express.json)
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next()
