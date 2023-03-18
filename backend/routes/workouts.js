989693d54b72e5cbf7bbdd1b2a95e7857d009754
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-18 18:11:14 +0100

    New commit

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,9 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts
+} = require('../controllers/workoutControllers')
 
 const router = express.Router()
 
@@ -13,16 +17,7 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', async(req, res) => {
-    const {title, load, reps} = req.body
-
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch(error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
