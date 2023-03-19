5503abb60bd403e7b8d0c0500d7f692edcec7643
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-19 22:10:07 +0100

    New commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fectchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fectchWorkouts()
+    }, [])
+
   return (
     <div className="home">
-        <h2>Home</h2>
+    {workouts && workouts.map((workout) => (
+        <p key={workout.id}>{workout.title}</p>
+    ))}
     </div>
   )
 }
