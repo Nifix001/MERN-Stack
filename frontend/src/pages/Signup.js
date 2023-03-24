2da77fac090a8d15da964f8141718af61fee749f
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-24 15:32:17 +0100

    New commit

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,7 +4,7 @@ import { useSignup } from '../hooks/useSignup'
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {signup, error, isloading} = useSignup()
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) =>{
         e.preventDefault()
@@ -28,7 +28,7 @@ const Signup = () => {
          onChange={(e) => setPassword(e.target.value)}
          value={password}
          />
-         <button>Sign up</button>
+         <button disabled={isLoading}>Sign up</button>
          {error && <div className='error'>{error}</div>}
     </form>
   )
