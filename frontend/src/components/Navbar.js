fb256c5b8a9be911c5a89fc4b3d32d105fe45a57
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-24 22:51:44 +0100

    New commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
+import { useAuthContext } from "../hooks/useAuthContext"
 import { useLogout } from "../hooks/useLogout"
 
 const Navbar = () => {
 
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleclick =() => {
     logout()
@@ -16,13 +18,18 @@ const Navbar = () => {
             <h1>Workout Buddy</h1>
         </Link>
         <nav>
+          {user &&(
           <div>
+            <span>{user.email}</span>
             <button onClick={handleclick}>Log Out</button>
           </div>
+          )}
+          {!user && (
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
           </div>
+          )}
         </nav>
       </div>
     </header>
